a301c46817fd21607a273c12ab765648c189ea19
user <user@user-PC4>
2017-12-28 15:39:48 +0900

    ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ

--- GItTest/src/TEst.java
+++ GItTest/src/TEst.java
@@ -7,7 +7,15 @@ public class TEst {
 		String[] array = {"동국", "혜린", "의현", "희진", "정환"};
 			for (int j = 0; j < array.length; j++) {
 				System.out.print(array[j] + "");
+			
 		}
-
+			
+			String man = "영동";
+			String man1 = "재우씨";
+			if(man.equals(man1)) {
+				System.out.println("여러분의 생각은?");
+			}else
+				System.out.println("답변하기 힘들어용");
+			
 	}
 }
