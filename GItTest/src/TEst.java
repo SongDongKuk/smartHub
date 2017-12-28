3031acb9c62f6bbc420aa5ca161edb64c57efd4c
user <user@user-PC7>
2017-12-28 16:01:57 +0900

    이거 대답이에용

--- GItTest/src/TEst.java
+++ GItTest/src/TEst.java
@@ -6,6 +6,7 @@ public class TEst {
 		
 
 		System.out.println("동국이형 짱잘생겼어요");
+		System.out.println("의현이가 더 잘생겼어요");
 		String[] array = {"동국", "혜린", "의현", "희진", "정환"};
 			for (int j = 0; j < array.length; j++) {
 				System.out.print(array[j] + "");
@@ -13,7 +14,7 @@ public class TEst {
 		}
 
 		System.out.println("우리 주말에 뭐해요?");
-
+		System.out.println("주말에 아이디어 회의하고 밥도먹고 저녁에 좀 놀다가 집가장");
 
 			
 			String man = "영동";
@@ -26,6 +27,7 @@ public class TEst {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("집에갈까");
 		}
+		System.out.println("집가장");
 			
 	}
 }
