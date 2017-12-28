45ff0ee02f75cb6d5821263c1c0a2f66ff126dd8
user <user@user-PC7>
2017-12-28 15:28:14 +0900

    그냥해봤어용

--- GItTest/src/TEst.java
+++ GItTest/src/TEst.java
@@ -4,5 +4,9 @@ public class TEst {
 		System.out.println("hello");
 		System.out.println("hello2");
 		System.out.println("동국이형 잘생겼어요");
+		String[] array = {"동국", "혜린", "의현", "희진", "정환"};
+			for (int j = 0; j < array.length; j++) {
+				System.out.print(array[j] + "");
+		}
 	}
 }
