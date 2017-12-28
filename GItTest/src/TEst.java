99e7b2c7f2d10730e7ffee989771272f24da4151
user <user@user-PC5>
2017-12-28 15:59:37 +0900

    짱

--- GItTest/src/TEst.java
+++ GItTest/src/TEst.java
@@ -5,7 +5,7 @@ public class TEst {
 
 		
 
-		System.out.println("동국이형 잘생겼어요");
+		System.out.println("동국이형 짱잘생겼어요");
 		String[] array = {"동국", "혜린", "의현", "희진", "정환"};
 			for (int j = 0; j < array.length; j++) {
 				System.out.print(array[j] + "");
